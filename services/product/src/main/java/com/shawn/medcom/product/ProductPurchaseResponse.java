25086f3d86a972f029b48d7ad8c53ef7f3f18821
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-12-06 20:16:56 +0530

    implemented product ms, exception handling done. Also, created test data using flywaymig

---
+++ services/product/src/main/java/com/shawn/medcom/product/ProductPurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.shawn.medcom.product;
+
+import java.math.BigDecimal;
+
+public record ProductPurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
